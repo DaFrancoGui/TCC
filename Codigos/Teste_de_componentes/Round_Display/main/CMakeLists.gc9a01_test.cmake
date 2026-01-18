c105f35890142201f6be216cfb0141c38cff4d20
dafranco <guilherme.franco@hexagon.com>
2026-01-18 15:22:27 -0300

    Adicionando fast cmakes pro round display

---
+++ Codigos/Teste_de_componentes/Round_Display/main/CMakeLists.gc9a01_test.cmake
@@ -0,0 +1,3 @@
+idf_component_register(SRCS "main_gc9a01_test.c"
+                    INCLUDE_DIRS "."
+                    REQUIRES driver gc9a01)
---
+++ Codigos/Teste_de_componentes/Round_Display/main/CMakeLists.touch_clone.cmake
@@ -0,0 +1,3 @@
+idf_component_register(SRCS "main_touch_clone.c"
+                    INCLUDE_DIRS "."
+                    REQUIRES driver gc9a01)
